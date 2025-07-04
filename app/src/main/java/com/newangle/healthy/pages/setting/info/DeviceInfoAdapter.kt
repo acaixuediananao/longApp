42344733efe3c63a1874f870157769ad964ef9b5
wxl22chong <wxl22chong@126.com>
2025-07-04 15:50:32 +0800

    优化

--- app/src/main/java/com/newangle/healthy/pages/setting/info/DeviceInfoAdapter.kt
+++ app/src/main/java/com/newangle/healthy/pages/setting/info/DeviceInfoAdapter.kt
@@ -7,7 +7,7 @@ import com.newangle.healthy.bean.DeviceInfo
 import com.newangle.healthy.databinding.DeviceInfoItemLayoutBinding
 import okhttp3.internal.notify
 
-class DeviceInfoAdapter(private val data : MutableList<DeviceInfo> = mutableListOf<DeviceInfo>())
+class DeviceInfoAdapter(private var data : List<DeviceInfo> = emptyList())
     : RecyclerView.Adapter<DeviceInfoAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -22,20 +22,20 @@ class DeviceInfoAdapter(private val data : MutableList<DeviceInfo> = mutableList
         position: Int
     ) {
         var deviceInfo = data[position]
-        holder.mBinding.deviceInfoItemCardDisplayName.text = deviceInfo.showName
-        holder.mBinding.deviceInfoItemCardDisplayValue.text = deviceInfo.value
+        holder.displayName.text = deviceInfo.showName
+        holder.displayValue.text = deviceInfo.value
     }
 
     override fun getItemCount(): Int = data.size
 
     fun setData(newData : List<DeviceInfo>) {
-        data.apply {
-            clear()
-            addAll(newData)
-        }
+        data = newData.toList()
         notifyDataSetChanged()
     }
 
-    inner class ViewHolder(val mBinding: DeviceInfoItemLayoutBinding) : RecyclerView.ViewHolder(mBinding.root)
+    class ViewHolder(val mBinding: DeviceInfoItemLayoutBinding) : RecyclerView.ViewHolder(mBinding.root) {
+        val displayName = mBinding.deviceInfoItemCardDisplayName
+        val displayValue = mBinding.deviceInfoItemCardDisplayValue
+    }
 
 }
