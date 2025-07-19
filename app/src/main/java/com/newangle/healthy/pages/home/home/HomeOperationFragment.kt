22861d9b9fc6d398b6ff9bd24819cbf9b4237cf0
wxl22chong <wxl22chong@126.com>
2025-07-19 17:50:38 +0800

    简单的交互

--- app/src/main/java/com/newangle/healthy/pages/home/home/HomeOperationFragment.kt
+++ app/src/main/java/com/newangle/healthy/pages/home/home/HomeOperationFragment.kt
@@ -1,26 +1,69 @@
 package com.newangle.healthy.pages.home.home
 
 import android.content.Context
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.appcompat.widget.DrawableUtils
+import androidx.appcompat.widget.ThemeUtils
+import androidx.compose.ui.res.stringResource
 import com.newangle.healthy.NewAngleApp
+import com.newangle.healthy.R
 import com.newangle.healthy.base.BaseFragment
 import com.newangle.healthy.base.logger.LogUtils
 import com.newangle.healthy.databinding.HomeOperationFragmentLayoutBinding
 import com.newangle.healthy.di.fragment.FragmentComponent
-import com.orhanobut.logger.Logger
 import javax.inject.Inject
 
 class HomeOperationFragment : BaseFragment() {
 
     @Inject
     lateinit var viewModel: HomeOperationViewModel
-    private lateinit var binding: HomeOperationFragmentLayoutBinding
+    private var _binding: HomeOperationFragmentLayoutBinding? = null
+    private val binding get() = _binding!!
 
     private val fragmentComponent: FragmentComponent by lazy {
         ((requireActivity().application) as NewAngleApp).appComponent.fragmentComponent().create()
+    }
+
+    private val onClickListener = View.OnClickListener {
+        LogUtils.i("click button")
+        when (it.id) {
+            binding.frequencyValueUpButton.id -> {
+                LogUtils.i("increase frequency ${viewModel.frequency.value}")
+                viewModel.increaseFrequency()
+            }
+            binding.frequencyValueDownButton.id -> {
+                LogUtils.i("decrease frequency ${viewModel.frequency.value}")
+                viewModel.decreaseFrequency()
+            }
+            binding.energyValueUpButton.id -> {
+                LogUtils.i("increase energy ${viewModel.energy.value}")
+                viewModel.increaseEnergy()
+            }
+            binding.energyValueDownButton.id -> {
+                LogUtils.i("decrease energy ${viewModel.energy.value}")
+                viewModel.decreaseEnergy()
+            }
+            binding.pulseValueUpButton.id -> {
+                LogUtils.i("increase pulse ${viewModel.pulse.value}")
+                viewModel.increasePulse()
+            }
+            binding.pulseValueDownButton.id -> {
+                LogUtils.i("decrease pulse ${viewModel.pulse.value}")
+                viewModel.decreasePulse()
+            }
+            binding.selectGenderIcon.id -> {
+                viewModel.changeGender()
+            }
+            binding.startButtonLayout.id -> {
+                viewModel.changeLaunchState()
+            }
+        }
     }
 
     override fun onAttach(context: Context) {
@@ -34,9 +77,69 @@ class HomeOperationFragment : BaseFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = HomeOperationFragmentLayoutBinding.inflate(inflater, container, false)
+        _binding = HomeOperationFragmentLayoutBinding.inflate(inflater, container, false)
         LogUtils.i("weixiaolong---- home operation fragment on create view  ${System.currentTimeMillis()}")
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupView()
+        setupViewModel()
+    }
+
+    private fun setupView() {
+        with(binding) {
+            frequencyValueUpButton.setOnClickListener(onClickListener)
+            frequencyValueDownButton.setOnClickListener(onClickListener)
+            energyValueUpButton.setOnClickListener(onClickListener)
+            energyValueDownButton.setOnClickListener(onClickListener)
+            pulseValueUpButton.setOnClickListener(onClickListener)
+            pulseValueDownButton.setOnClickListener(onClickListener)
+            selectGenderIcon.setOnClickListener(onClickListener)
+            startButtonLayout.setOnClickListener(onClickListener)
+        }
+    }
+
+    private fun setupViewModel() {
+        viewModel.frequency.observe(viewLifecycleOwner, ::updateFrequency)
+        viewModel.energy.observe(viewLifecycleOwner, ::updateEnergy)
+        viewModel.pulse.observe(viewLifecycleOwner, ::updatePulse)
+        viewModel.gender.observe(viewLifecycleOwner, ::updateGender)
+        viewModel.launchState.observe(viewLifecycleOwner, ::updateLaunchState)
+    }
+
+    private fun updateFrequency(value : Int) {
+        binding.frequencyValue.text = value.toString()
+    }
+
+    private fun updateEnergy(value : Int) {
+        binding.energyValue.text = value.toString()
+    }
+
+    private fun updatePulse(value : Int) {
+        binding.pulseValue.text = value.toString()
+    }
+
+    private fun updateGender(gender : HomeOperationViewModel.Gender) {
+        when (gender) {
+            HomeOperationViewModel.Gender.MALE -> {
+                binding.selectedGenderFigure.setImageResource(R.drawable.gender_male_figure)
+            }
+            HomeOperationViewModel.Gender.FEMALE -> {
+                binding.selectedGenderFigure.setImageResource(R.drawable.gender_female_figure)
+            }
+        }
+    }
+
+    private fun updateLaunchState(value : Boolean) {
+        binding.startButton.text = if (value) getString(R.string.start_work_state_stop) else getString(R.string.start_work_state_start)
+            val endDrawable =
+                if (value)
+                    AppCompatResources.getDrawable(requireActivity(), R.drawable.pause_state)
+                else
+                    AppCompatResources.getDrawable(requireActivity(), R.drawable.start_state)
+        binding.startButton.setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, endDrawable, null)
     }
 
     override fun onResume() {
@@ -44,12 +147,16 @@ class HomeOperationFragment : BaseFragment() {
         LogUtils.i("weixiaolong---- home operation fragment on resume ${System.currentTimeMillis()}")
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
     companion object {
-        fun newInstance(): HomeOperationFragment {
-            val args = Bundle()
-            val fragment = HomeOperationFragment()
-            fragment.arguments = args
-            return fragment
+        fun newInstance() = HomeOperationFragment().apply {
+            arguments = Bundle().apply {
+
+            }
         }
     }
 }
--- app/src/main/java/com/newangle/healthy/pages/home/home/HomeOperationViewModel.kt
+++ app/src/main/java/com/newangle/healthy/pages/home/home/HomeOperationViewModel.kt
@@ -2,11 +2,71 @@ package com.newangle.healthy.pages.home.home
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.newangle.healthy.di.fragment.FragmentComponent
 import javax.inject.Inject
 
 class HomeOperationViewModel @Inject constructor(application: Application, fragmentComponent: FragmentComponent) : AndroidViewModel(application) {
+
+    private val _frequency = MutableLiveData(DEFAULT_FREQUENCY)
+    val frequency: LiveData<Int> get() = _frequency
+    private val _energy = MutableLiveData(DEFAULT_ENERGY)
+    val energy: LiveData<Int> get() = _energy
+    private val _pulse = MutableLiveData(DEFAULT_PULSE)
+    val pulse: LiveData<Int> get() = _pulse
+    private val _gender = MutableLiveData<Gender>(Gender.FEMALE)
+    val gender: LiveData<Gender> get() = _gender
+    private val _launchState = MutableLiveData(false)
+    val launchState get() = _launchState
+
     init {
         fragmentComponent.inject(this)
+    }
+
+    fun increaseFrequency() = _frequency.safeUpdate(max = MAX_FREQUENCY) { it + 1 }
+
+    fun decreaseFrequency() = _frequency.safeUpdate(min = MIN_FREQUENCY) { it - 1 }
+
+    fun increaseEnergy() = _energy.safeUpdate(max = MAX_ENERGY) { it + 1 }
+
+    fun decreaseEnergy() = _energy.safeUpdate(min = MIN_ENERGY) { it - 1 }
+
+    fun increasePulse() = _pulse.safeUpdate(max = MAX_PULSE) { it + 1 }
+
+    fun decreasePulse() = _pulse.safeUpdate(min = MIN_PULSE) { it - 1 }
+
+    fun changeGender() {
+        _gender.value = if (_gender.value == Gender.MALE) Gender.FEMALE else Gender.MALE
+    }
+
+    fun changeLaunchState() {
+        _launchState.value.let {
+            _launchState.value = !it
+        }
+    }
+
+    private fun MutableLiveData<Int>.safeUpdate(max: Int = Integer.MAX_VALUE, min: Int = Integer.MIN_VALUE, update: (Int) -> Int ) {
+        value?.let { current ->
+            val newValue = update(current)
+            if (newValue in min..max) value = newValue
+        }
+    }
+
+    companion object {
+        private const val MAX_FREQUENCY = 10
+        private const val MIN_FREQUENCY = 1
+        private const val DEFAULT_FREQUENCY =5
+        private const val MAX_ENERGY = 80
+        private const val MIN_ENERGY = 1
+        private const val DEFAULT_ENERGY = 30
+        private const val MAX_PULSE = 100
+        private const val MIN_PULSE = 1
+        private const val DEFAULT_PULSE = 60
+    }
+
+    sealed interface Gender {
+        object MALE : Gender
+        object FEMALE : Gender
     }
 }
